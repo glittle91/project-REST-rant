91f68bc09fe22045d8123db773d7410f9412491f
Gabrielle Little <gabriellelittle@gabrielles-mbp.mynetworksettings.com>
2022-06-15 18:51:56 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+// Load up modules
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Configure middlewares
+app.use('/places', require('./controllers/places'))
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+// Listen to requests
+app.listen(process.env.PORT)

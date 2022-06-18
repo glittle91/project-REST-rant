1c3b9e7e48b10fe9954b2848a1ee926c05a75135
Gabrielle Little <gabriellelittle@gabrielles-mbp.mynetworksettings.com>
2022-06-18 13:43:15 -0400

    Nav bar with style

--- views/default.jsx
+++ views/default.jsx
@@ -8,6 +8,19 @@ function Def (html) {
                 <link rel="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" href="/css/style.css"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>

f3c85bbca9b1bafba2fdd096bb354a7985842c68
Gabrielle Little <gabriellelittle@gabrielles-mbp.mynetworksettings.com>
2022-06-18 06:48:04 -0400

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,17 @@
-const React = require ('react')
+const React = require('react')
+
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link rel="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-
- }
+}
 
 module.exports = Def
